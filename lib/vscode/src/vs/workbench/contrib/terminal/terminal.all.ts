9e358ec64f8b8d7aae5ad46a53235685613391a8
itsuki <otogi2025@gmail.com>
2026-09-09 10:50:19 +0900

    3-99 整包删掉 terminalContrib/telemetry：每开一个终端就挂一个 10 秒定时器和一组监听，只为发一条遥测关闭后必被丢弃的事件

--- lib/vscode/src/vs/workbench/contrib/terminal/terminal.all.ts
+++ lib/vscode/src/vs/workbench/contrib/terminal/terminal.all.ts
@@ -30,6 +30,5 @@ import '../terminalContrib/resizeDimensionsOverlay/browser/terminal.resizeDimens
 import '../terminalContrib/sendSequence/browser/terminal.sendSequence.contribution.js';
 import '../terminalContrib/sendSignal/browser/terminal.sendSignal.contribution.js';
 import '../terminalContrib/suggest/browser/terminal.suggest.contribution.js';
-import '../terminalContrib/telemetry/browser/terminal.telemetry.contribution.js';
 import '../terminalContrib/tomoshibiActivity/browser/terminal.tomoshibiActivity.contribution.js';
 import '../terminalContrib/tomoshibiTouchSelect/browser/terminal.tomoshibiTouchSelect.contribution.js';
